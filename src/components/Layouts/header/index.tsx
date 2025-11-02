8fddb44a882dcbbd754e5e3d059380dbc5772dc9
YinKuanL <87226880+YinKuanL@users.noreply.github.com>
2025-11-02 11:10:16 +0000

    Remove search icon

--- src/components/Layouts/header/index.tsx
+++ src/components/Layouts/header/index.tsx
@@ -40,10 +40,6 @@ export function Header() {
       </div>
 
       <div className="flex flex-1 items-center justify-end gap-2 min-[375px]:gap-4">
-        <div className="relative w-full max-w-[300px]">
-          <SearchIcon className="pointer-events-none absolute left-5 top-1/2 -translate-y-1/2 max-[1015px]:size-5" />
-        </div>
-
         <ThemeToggleSwitch />
       </div>
     </header>
